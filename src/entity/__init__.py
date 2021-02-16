b275f19498c5a09b9a254070bbf4c97ce2006f6a
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-02-16 09:04:51 -0600

    src.entity.entities added to _all_ for src.entity

--- src/entity/__init__.py
+++ src/entity/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ["Entity", "CannotMoveException", "NotOnPlayFieldException"]
+__all__ = ["Entity", "CannotMoveException", "NotOnPlayFieldException", "entities"]
 
 from typing import Optional, Tuple
 
 from src.entity.entity_ import Entity, CannotMoveException, NotOnPlayFieldException
-
+from src.entity import entities
