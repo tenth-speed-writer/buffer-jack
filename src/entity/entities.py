b4999c73ca16a1e2bf57c915abaa8d5ccd13df1f
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-02-15 18:45:08 -0600

    Project structure, adds Mobile and Static subclasses of Entity

---
+++ src/entity/entities.py
@@ -0,0 +1,37 @@
+from typing import Optional, Tuple
+from src.entity import Entity
+
+
+class Static(Entity):
+    """Static vs Mobile determines whether an Entity has the logic to move under its own prerogative.
+    A Static can still be moved, but will complain unless move_to is further overridden."""
+    def move_to(self, x, y) -> None:
+        print("Warning: using move_to on Static entity {} at (x:{}, y:{})")\
+            .format(self.name, str(x), str(y))
+        super().move_to(x, y)
+
+
+class Mobile(Entity):
+    """An entity which has both a base movement cost property and
+    an @property move_cost which can be overridden for game logic."""
+    def __init__(self,
+                 size: int,
+                 sigil: str,
+                 name: Optional[str] = None,
+                 parent_cell: Optional[object] = None,
+                 parent_playfield: Optional[object] = None,
+                 position: Optional[Tuple[int, int]] = None,
+                 sigil_priority: int = 3,
+                 base_move_cost: int = 100) -> None:
+        super().__init__(size=size, sigil=sigil, name=name,
+                         parent_cell=parent_cell,
+                         parent_playfield=parent_playfield,
+                         position=position,
+                         sigil_priority=sigil_priority)
+        self._base_move_cost = base_move_cost
+
+    @property
+    def move_cost(self):
+        """Returns number of ticks it costs move a tile on the map.
+        Overwrite this method to apply stat logic and buffs/maluses."""
+        return self._base_move_cost
---
+++ src/entity/entity_.py
@@ -0,0 +1,117 @@
+from typing import Optional, Tuple
+
+
+class CannotMoveException(Exception):
+    """Raised when an entity wants to move to a tile but can't.
+    Things should generally not try to move where they can't to begin with,
+    so when this appears in the log it probably indicates AI logic failure."""
+    pass
+
+
+class NotOnPlayFieldException(Exception):
+    """Raised when something tries to do something with an entity's position,
+    but the entity does not currently have one (i.e. it isn't on the playfield.)"""
+    pass
+
+
+class Entity:
+    """Anything which can be rendered in a PlayField, be it terrain or actor."""
+    def __init__(self,
+                 size: int,
+                 sigil: str,
+                 name: Optional[str] = None,
+                 parent_cell: Optional[object] = None,
+                 parent_playfield: Optional[object] = None,
+                 position: Optional[Tuple[int, int]] = None,
+                 sigil_priority: int = 3):
+        self._sigil = sigil
+        self._sigil_priority = sigil_priority
+        self._size = size
+        self._position = position
+        self._name = name
+
+        self._parent_cell = parent_cell
+        self._parent_playfield = parent_playfield
+
+    @property
+    def cell(self):
+        """Returns the cell in which the entity exists.
+        Raises a NotOnPlayFieldException if parent playfield and cell aren't both defined."""
+        if self._parent_cell and not self._parent_playfield:
+            raise NotOnPlayFieldException("Entity {} has a parent Cell, but not a parent PlayField."
+                                          .format(str(self._name)))
+        elif self._parent_cell:
+            return self._parent_cell
+        else:
+            raise NotOnPlayFieldException("Entity {} does not have a parent Cell.".format(str(self._name)))
+
+    @property
+    def playfield(self):
+        """Returns the PlayField in which the entity exists.
+        Raises a NotOnPlayFieldException if parent playfield and cell aren't both defined."""
+        if self._parent_playfield and not self._parent_cell:
+            raise NotOnPlayFieldException("Entity {} has a parent PlayField, but not a parent Cell."
+                                          .format(str(self._name)))
+        elif self._parent_playfield:
+            return self._parent_playfield
+        else:
+            raise NotOnPlayFieldException("Entity {} does not have a parent PlayField.".format(str(self._name)))
+
+    @playfield.setter
+    def playfield(self, pf):
+        """Sets the object's new playfield, assuming it passes some ducktype testing."""
+
+        # Simple test since we're limited by a circular import reference and can't use true type hinting. :(
+        assert hasattr(pf, "_field")
+        self._parent_playfield = pf
+
+    def _change_cell(self, c) -> None:
+        # TODO: Consider cells having a "can_accept" @property that we check here
+        self._parent_cell = c
+
+    @cell.setter
+    def cell(self, new_cell) -> None:
+        """Change this entity's cell, including changing its playfield if necessary.
+        This generally corresponds to the entity being moved or placed."""
+
+        self.playfield = new_cell.playfield
+        self._change_cell(new_cell)
+
+    @property
+    def position(self):
+        if not self._parent_playfield:
+            raise NotOnPlayFieldException("Entity {} does not have a parent PlayField.".format(str(self._name)))
+        elif not self._parent_cell:
+            raise NotOnPlayFieldException("Entity {} does not have a parent Cell.".format(str(self._name)))
+        else:
+            return self._parent_cell.position
+
+    @property
+    def sigil(self):
+        """Returns the class's sigil and that sigil's priority.
+        A higher priority means this sigil will be rendered over the others.
+        Level 3 is mobs and terrain, level 2 is stuff on the floor, level 4 is obscuring stuff like smoke."""
+        return self._sigil, self._sigil_priority
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def name(self) -> str:
+        return str(self._name)
+
+    def move_to(self, x, y) -> None:
+        """Get Cell x, y from the entity's playfield, then attempt to move the entity there.
+        The entity must already have a playfield in which it exists; otherwise, use introduce_at(x, y, playfield)"""
+
+        # Find the cell in question and reassign it, firing the
+        # above-written setter to handle both pf and cell changes
+        new_cell = self.playfield.get_cell(x, y)
+        self.cell = new_cell
+
+    def introduce_at(self, x, y, playfield) -> None:
+        """As per move_to, but assumes the Entity doesn't already have a playfield.
+        Use when spawning an entity on the playfield for the first time."""
+        self.playfield = playfield
+        self.cell = playfield.get_cell(x=x, y=y)
