c0f6744d4ff82497321024cd19b5d883cc962439
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-02-20 14:02:48 -0600

    More comprehensive tests for the Sigil class

--- src/tests/test_sigil.py
+++ src/tests/test_sigil.py
@@ -23,3 +23,48 @@ class TestSigil(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             test3 = Sigil("F2f")
+
+        with self.assertRaises(ValueError):
+            test4 = Sigil("C", color=(42, -85, 94))
+
+    def test_color(self):
+        test = Sigil("A", priority=3, color=(199, 198, 197))
+
+        assert test.r == 199
+        assert test.g == 198
+        assert test.b == 197
+
+        assert test.color == (199, 198, 197)
+
+        test.color = (101, 102, 103)
+
+        assert test.r == 101
+        assert test.g == 102
+        assert test.b == 103
+
+        test.b = 202
+        assert test.color[2] == 202
+
+        with self.assertRaises(ValueError):
+            test.g = 442
+
+        with self.assertRaises(ValueError):
+            test.g = -30
+
+    def test_priority(self):
+        """Tests getter & setter for Sigil.priority"""
+        test = Sigil("R")
+        test2 = Sigil("S", priority=2)
+        assert test.priority > test2.priority
+
+        test2.priority = 4
+        assert test2.priority > test.priority
+
+        with self.assertRaises(Exception):
+            test.priority = 6
+
+        with self.assertRaises(Exception):
+            test.priority = -3
+
+        with self.assertRaises(Exception):
+            test.priority = "Foo"
