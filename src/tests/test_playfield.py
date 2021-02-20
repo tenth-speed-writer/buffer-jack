5dca8ad86c0e4fed3fb8430910e414f6e8efb677
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-02-20 14:35:09 -0600

    Expands tests for playfield.Cell

--- src/tests/test_playfield.py
+++ src/tests/test_playfield.py
@@ -14,16 +14,19 @@ class TestCell(unittest.TestCase):
         ent2 = Entity(4, Sigil("B"))
         ent3 = Entity(4, Sigil("C", priority=2))
 
+        # Introduce three passable entities, one with a lower priority
         ent.introduce_at(2, 2, pf)
         ent2.introduce_at(2, 2, pf)
         ent3.introduce_at(2, 2, pf)
         sigs: List[Sigil] = pf.get_cell(2, 2).sigils
 
+        # Assert that the two highest, but not the one lowest, are returned by .sigil
         assert ent.sigil in sigs
         assert ent2.sigil in sigs
         assert ent3.sigil not in sigs
 
     def test_position(self):
+        """.position should return an (x, y) tuple of ints and reject assignment."""
         pf = PlayField(5, 5)
         c = pf.get_cell(x=2, y=3)
 
@@ -34,6 +37,7 @@ class TestCell(unittest.TestCase):
             c.position = 2, 2
 
     def test_passable(self):
+        """Should be passable if no entity in the cell has .passable=False"""
         ent1 = Entity(4, Sigil("A"))
         ent2 = Entity(3, Sigil("B"))
         ent3 = Entity(3, Sigil("C"))
@@ -54,7 +58,13 @@ class TestCell(unittest.TestCase):
         assert c.passable
 
     def test_playfield(self):
-        pass
+        """.playfield should return the Cell's playfield and reject assignment."""
+        pf = PlayField(2, 3)
+        c = pf.get_cell(1, 2)
+        assert c.playfield is pf
+
+        with self.assertRaises(Exception):
+            c.playfield = PlayField(5, 5)
 
 
 class TestPlayField(unittest.TestCase):
