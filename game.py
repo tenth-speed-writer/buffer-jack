692be34225b22090049fae6a4ca45501163818de
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-02-21 17:50:19 -0600

    We have a dude moving on a field. Now let's work out how to it right, and we'll have a game engine.

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import tcod
-from math import floor
 from src.menus import Menu, MenuOption
 from src.inputs.gameplay import GameplayHandler
 from src.playfield import PlayField
-from typing import List, Tuple, Optional
+from src.entity.entities import Mobile
+from src.sigil import Sigil
+from typing import List, Tuple, Optional, Dict
 
 # Default window resolution
 WIDTH, HEIGHT = 720, 480
@@ -31,30 +32,56 @@ def main():
         main_menu.add_option(MenuOption(text="Launch\nGame",
                                         width=min(console.width - 6, 14),  # Ideally 14, but shrink to fit if needed.
                                         height=5,
-                                        on_select=lambda x: print("Player clicked the button!"),
+                                        on_select=lambda: print("Player clicked the button!"),
                                         color=(130, 220, 130)))
-        menus: List[Tuple[int, int, Menu]] = [(1, 1, main_menu)]
+        menus: Optional[List[Tuple[int, int, Menu]]] = None  # [(1, 1, main_menu)]
 
         # The variable which will hold the active playfield
-        playfield: Optional[PlayField] = None
-        dispatcher: Optional[tcod.event.EventDispatch] = None
+
+        player_char = Mobile(size=4,
+                             sigil=Sigil("@"),
+                             name="Player Character")
+        playfield: Optional[PlayField] = PlayField(40, 40)
+        player_char.introduce_at(10, 10, playfield)
+        playfield.dispatch = GameplayHandler(playfield, player_char)
 
         while True:
-            console = context.new_console(order="F")
+            # Create the console and draw
+            console = context.new_console(order="F", min_columns=32, min_rows=24)
             console.draw_frame(x=0, y=0,
                                width=console.width,
                                height=console.height,
                                title="BUFFER.JACK()")
+
             # Render the playfield, if one is open
+            if playfield:
+                playfield.tick()
+                pf_rows: List[Dict] = playfield.drawables()
+                for r in pf_rows:
+                    console.print(x=r["x"],
+                                  y=r["y"],
+                                  string=r["character"],
+                                  fg=r["rgb"])
 
             if menus:
                 # Render each menu that's currently open
                 for _x, _y, _menu in menus:
                     _menu.render_menu(_x, _y, console)
                 top_menu = menus[-1][2]  # The topmost menu will be third element of the last tuple in the list
-                dispatcher = top_menu.
+                dispatcher: tcod.event.EventDispatch = top_menu.dispatch
 
                 # Hand event control to the last
+                for event in tcod.event.get():
+                    dispatcher.dispatch(event)
+            else:
+                # If no menu is open, but the playfield is, then use its event handler
+                dispatcher: tcod.event.EventDispatch = playfield.dispatch
+
+            context.present(console)
+            for event in tcod.event.get():
+                context.convert_event(event)  # Converting the event fills in mouse event tile coordinates
+                dispatcher.dispatch(event)
+
 
 
         # HANDLING MENUS:
