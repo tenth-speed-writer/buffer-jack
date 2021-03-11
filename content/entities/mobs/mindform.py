38f77318951840dba2dd20f9da1a3cbb77370b92
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-03-11 13:56:40 -0600

    Clean-up and comments

--- content/entities/mobs/mindform.py
+++ content/entities/mobs/mindform.py
@@ -1,7 +1,7 @@
 from src.sigil import Sigil
 from src.entity.entities import Mobile
 from src.modifiers import Modifier, MultiplicativeModifier, AdditiveModifier
-from typing import List, Dict
+from typing import List
 
 
 class MindForm(Mobile):
@@ -17,19 +17,19 @@ class MindForm(Mobile):
         def __check_args():
             """Wraps up the argument tests for MindForm generation."""
             if not (1 <= recognizance <= 100):
-                raise ValueError("Argument 'recognizance' must be an integer in between 1 and 100")
+                raise ValueError("Argument 'recognizance' must be an float in between 1 and 100")
 
             elif not (1 <= deconstruction <= 100):
-                raise ValueError("Argument 'deconstruction' must be an integer in between 1 and 100")
+                raise ValueError("Argument 'deconstruction' must be an float in between 1 and 100")
 
             elif not (1 <= attention <= 100):
-                raise ValueError("Argument 'attention' must be an integer in between 1 and 100")
+                raise ValueError("Argument 'attention' must be an float in between 1 and 100")
 
             elif not (1 <= resolution <= 100):
-                raise ValueError("Argument 'resolution' must be an integer between 1 and 100")
+                raise ValueError("Argument 'resolution' must be an float between 1 and 100")
 
             elif not (1 <= empathy <= 100):
-                raise ValueError("Argument 'empathy' must be an integer between 1 and 100")
+                raise ValueError("Argument 'empathy' must be an float between 1 and 100")
 
         __check_args()
         super().__init__(name=name,
