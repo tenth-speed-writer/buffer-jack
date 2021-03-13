4cf4e0023e6e4cefc138b4fdc9f127afbbf58bae
A. L. Seale TenthSpeedWriter <addseale2@gmail.com>
2021-03-13 14:34:49 -0600

    Adds overrides and hooks for on_create, on_destroy, on_move, and on_just_moved

--- content/entities/mobs/fooform.py
+++ content/entities/mobs/fooform.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple, Optional
 from src.modifiers import Modifier, MultiplicativeModifier, AdditiveModifier
 from src.entity.entities import Mobile
 
@@ -13,7 +13,9 @@ class FooForm(Mobile):
         if stat not in self.modifiers.keys():
             raise ValueError("Entity's .modifiers dict has no value for key {}!".format(str(stat)))
 
-        modifiers: List[Modifier] = self.modifiers[stat] if stat in self.modifiers.keys() else []
+        modifiers: List[Modifier] = self.modifiers[stat]\
+            if stat in self.modifiers.keys()\
+            else []
 
         # Separate out modifiers by additive or multiplicative
         adds = [m for m in modifiers
@@ -38,3 +40,42 @@ class FooForm(Mobile):
         else:
             raise ValueError("Base stat {} must be a value between 0 and 100. Got {}"
                              .format(str(stat_name), str(value)))
+
+    def on_introduce(self) -> None:
+        """Overridable for logic which fires just after the object is introduced on the playfield."""
+        pass
+
+    def introduce_at(self, x, y, playfield) -> None:
+        """Introduces this FooForm onto the playfield, then fires any on_create method it might have been assigned."""
+        super().introduce_at(x, y, playfield)
+        self.on_introduce()
+
+    def on_destroy(self) -> None:
+        """Overridable for logic which fires just before this object is destroyed."""
+        pass
+
+    def destroy(self):
+        """Fires this FooForm's on_destroy method, then--as expected--destroys it."""
+        self.on_destroy()
+        super().destroy()
+
+    def on_move(self,
+                old_pos: Tuple[int, int],
+                new_pos: Tuple[int, int]) -> None:
+        """Overridable logic that fires just before this entity moves."""
+        pass
+
+    def on_just_moved(self,
+                      old_pos: Tuple[int, int],
+                      new_pos: Tuple[int, int]) -> None:
+        """Overridable logic that fires just after this entity moves."""
+        pass
+
+    def move_to(self, x, y) -> None:
+        """Fires self.on_move, then super().move_to, then self.on_just_moved."""
+        old = self.position
+        new = (x, y)
+
+        self.on_move(old, new)
+        super().move_to(x, y)
+        self.on_just_moved(old, new)
